a2774669d96486595aab6e4ca35cf12dae9fe55b
salim84miya <salimsaudagar84@gmail.com>
2023-11-03 20:04:31 +0530

    first day of DSA

---
+++ Chapter7.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class Chapter7  {
+    public static void main(String[] args){
+        
+        Scanner in = new Scanner(System.in);
+
+        int[] nums = new int[5];
+        
+        //using for loop for input
+        for (int i=0; i<nums.length; i++){
+            nums[i] = in.nextInt();
+        }
+
+        //using for loop to iterate and print the array
+        for(int i=0; i<nums.length; i++){
+            System.out.print(nums[i]+" ");
+        }
+
+        System.out.println();
+
+        //using enhanced for loop or for each loop to iterate & print the array
+        for (int i : nums) {
+            System.out.print(i+" ");
+        } 
+
+          System.out.println();
+
+        //using Arrays.toString to iterate the array and print the array
+
+        System.out.println(Arrays.toString(nums));
+    }
+} 
