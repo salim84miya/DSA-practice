3cc94fde4ecbd3135aad98d2980287ec9be2d847
salim84miya <salimsaudagar84@gmail.com>
2023-11-04 22:22:56 +0530

    learned multiarrayList

--- MultiArrayList.java
+++ MultiArrayList.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class MultiArrayList {
 
     public static void main(String[] args) {
-        
+       
+        Scanner in = new Scanner(System.in);
+
         /* 
          Here you dont have to specifies size of the array
          we can add as much element we want 
@@ -30,10 +33,36 @@ public class MultiArrayList {
            System.out.println(list);
 
            //for getting a value at specific index we use get method in list
-           for(int i=0; i<4; i++){
+          
+           /* for(int i=0; i<4; i++){
             System.out.println(list.get(i));
             
-           }
+           } */
+           
+
+           /*
+            The two dimensional arraylist is an arraylist of arraylist
+            the data type itself is arraylist here
+             */
+
+             ArrayList<ArrayList<Integer>> list2 = new ArrayList<>();
+
+
+             //first we have to initiazed the list else we will get error
+             for(int i=0; i<3; i++){
+                list2.add(new ArrayList<>());
+            }
+
+         /*    inputting the 2D arraylist first using get method to get the inner ararylist
+             and then add to insert element into that list */
+             for(int i=0; i<3; i++){
+                for(int j=0; j<3; j++){    
+                list2.get(i).add(in.nextInt());
+             }
+            }
+
+            //printing the list
+            System.out.println(list2);
         
     }
     
