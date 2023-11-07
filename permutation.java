5255f04a852364eb24df8c8556743056dd6c7aaa
salim84miya <salimsaudagar84@gmail.com>
2023-11-07 18:38:26 +0530

    made running sum problem's solution more efficient

--- permutation.java
+++ permutation.java
@@ -30,12 +30,10 @@ public class permutation {
             
             public static int[] runningSum(int[] nums) {
                 int[] ans = new int[nums.length];
+                int temp =0;
                 for(int i=0; i<nums.length; i++){
-                   int temp = 0;
-                    for(int j=0; j<=i; j++){
-                        temp += nums[j];
-                    }
-                    ans[i]= temp;
+                        temp += nums[i];
+                        ans[i]= temp;
                 }
                 return ans;
             }
