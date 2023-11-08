e0ab976c4be6f2ad5b40b6be3d49fbe5408a9e97
salim84miya <salimsaudagar84@gmail.com>
2023-11-08 17:56:16 +0530

    solved richest wealth problem

--- permutation.java
+++ permutation.java
@@ -3,10 +3,12 @@ import java.util.Arrays;
 public class permutation {
     public static void main(String[] args){
         int[] nums = {0,2,1,5,3,4};
-        int [] nums1 ={1,2,1};
-         System.out.println(Arrays.toString(buildArray(nums)));
+        int[] nums1 ={1,2,1};
+        int[][] nums2 = {{2,8,7},{7,1,3},{1,9,5}};
+        System.out.println(Arrays.toString(buildArray(nums)));
         System.out.println(Arrays.toString(getConcatenation(nums1)));
         System.out.println(Arrays.toString(runningSum(nums1)));
+        System.out.println(maximumWealth(nums2));
         
     }
     public static int[] buildArray(int[] nums) {
@@ -38,5 +40,24 @@ public class permutation {
                 return ans;
             }
         
+            public static int maximumWealth(int[][] accounts) {
+
+                int[] ans  = new int[accounts.length];
+                
+                    for(int i=0; i<accounts.length; i++){
+                        int temp =0;
+                        for(int j=0; j<accounts[i].length; j++){
+                            temp += accounts[i][j];
+                        }
+                        ans[i] = temp;
+                    }
+                    int max = ans[0];
+                    for(int i=0; i<accounts.length; i++){
+                        if(ans[i]>max){
+                            max = ans[i];
+                        }
+                    }
+                    return max ;
+                }
     
 }
